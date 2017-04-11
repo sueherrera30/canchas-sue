4f7447aa0f1749fe655eb805d93d1392a8b3d038
sue martinez <sueherrera30@gmail.com>
2017-04-11 10:47:35 +0000

    version medidas corregidas, sin cancha de tennis

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
 var botonBasket =document.getElementById('basket');
+var botonFut= document.getElementById('fut');
+// var botonTennis = document.getElementById('tennis');
 
+botonFut.addEventListener("click",mostrarFut);
 botonBasket.addEventListener("click", mostrarBasket);
+// botonTennis.addEventListener("click",mostrarTennis);
 
 function mostrarBasket(){
- document.getElementById('basketball').style.display = 'block';
+ document.getElementById('basketball').style.display ="block";
+ document.getElementById('futball').style.display ="none";
 }
-mostrarBasket();
+
+function mostrarFut(){
+  document.getElementById('futball').style.display ="block";
+ document.getElementById('basketball').style.display ="none";
+
+}
