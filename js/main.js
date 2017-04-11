83a6d84043cd5ad344d79cc51868a1a86250eb14
sue martinez <sueherrera30@gmail.com>
2017-04-11 01:08:38 +0000

    con cancha de futball desajustada e intento de funcionalidad

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var botonBasket =document.getElementById('basket');
+
+botonBasket.addEventListener("click", mostrarBasket);
+
+function mostrarBasket(){
+ document.getElementById('basketball').style.display = 'block';
+}
+mostrarBasket();
