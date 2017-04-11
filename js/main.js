d3843b5a6389dacfc48150c5fd941f4f0b647a01
sue martinez <sueherrera30@gmail.com>
2017-04-11 11:45:00 +0000

    version final basica

--- js/main.js
+++ js/main.js
@@ -1,18 +1,24 @@
 var botonBasket =document.getElementById('basket');
 var botonFut= document.getElementById('fut');
-// var botonTennis = document.getElementById('tennis');
+var botonTennis = document.getElementById('ten');
 
 botonFut.addEventListener("click",mostrarFut);
 botonBasket.addEventListener("click", mostrarBasket);
-// botonTennis.addEventListener("click",mostrarTennis);
+botonTennis.addEventListener("click",mostrarTennis);
 
 function mostrarBasket(){
  document.getElementById('basketball').style.display ="block";
  document.getElementById('futball').style.display ="none";
+ document.getElementById('tennis').style.display ="none";
 }
 
 function mostrarFut(){
   document.getElementById('futball').style.display ="block";
  document.getElementById('basketball').style.display ="none";
-
+document.getElementById('tennis').style.display ="none";
+}
+function mostrarTennis(){
+  document.getElementById('tennis').style.display ="block";
+ document.getElementById('basketball').style.display ="none";
+ document.getElementById('futball').style.display ="none";
 }
